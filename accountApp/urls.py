11eae578c720d4b3df25952c8ede84e7ec387ae7
yubin <itqkrdbqls1001@naver.com>
2020-11-04 03:30:09 +0900

    accountApp template/CSS 생성 / url 연결

--- accountApp/urls.py
+++ accountApp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path("login", views.login_view, name="login"),
+    path("mypage", views.mypage_view, name="mypage"),
+    path("select_user_type", views.select_user_type_view, name="select_user_type"),
+    path("signup_customer", views.signup_customer_view, name="signup_customer"),
+    path("signup_tattooist", views.signup_tattooist_view, name="signup_tattooist"),
 ]
--- accountApp/views.py
+++ accountApp/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# 로그인 페이지
+# 이미 로그인한 상태인지 확인해야 함
+# account/login
+def login_view(request):
+    return render(request, "login.html")
+
+
+# 마이페이지
+# account/mypage
+@login_required(login_url="/account/login")
+def mypage_view(request):
+    return render(request, "mypage.html")
+
+
+# 회원가입 전 유저 유형을 선택하는 페이지(시술자: tattooist / 피시술자: customer)
+# 이미 로그인한 상태인지 확인해야 함
+# account/select_user_type
+def select_user_type_view(request):
+    return render(request, "select_user_type.html")
+
+
+# 피시술자(customer) 회원가입 페이지
+# 이미 로그인한 상태인지 확인해야 함
+# account/signup_customer
+def signup_customer_view(request):
+    return render(request, "signup_customer.html")
+
+
+# 시술자(tattooist) 회원가입 페이지
+# 이미 로그인한 상태인지 확인해야 함
+# account/signup_tattooist
+def signup_tattooist_view(request):
+    return render(request, "signup_tattooist.html")
