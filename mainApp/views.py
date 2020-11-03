295bd1e4b8c7bbb185b8a2ad0a5c882cddf06ebe
yubin <itqkrdbqls1001@naver.com>
2020-11-04 03:16:23 +0900

    mainApp view에 주석 추가

--- mainApp/views.py
+++ mainApp/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+# 인트로 페이지
+# /
 def intro_view(request):
     return render(request, "intro.html")
 
+# 메인 페이지
+# /main
 def main_view(request):
     return render(request, "main.html")
