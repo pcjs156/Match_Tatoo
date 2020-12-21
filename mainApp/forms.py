dc548939e709b3dbe74aeea35d97dbf6b315275b
yubin <itqkrdbqls1001@naver.com>
2020-12-21 20:39:28 +0900

    report 구현

--- mainApp/forms.py
+++ mainApp/forms.py
@@ -1,4 +1,13 @@
 from django import forms
 
+from .models import Report
+
+
 class SearchForm(forms.Form):
     search_word = forms.CharField(label='search_word')
+
+
+class ReportForm(forms.ModelForm):
+    class Meta:
+        model = Report
+        fields = ['description']
